70a8dc4251095bdbff7494246be5a207d0b8db57
Alisson Macedo <alissontecnico@gmail.com>
2020-08-31 00:08:30 -0300

    [TA-00] - desafio-fundamentos-nodejs - Cadastro de valores de income e outcome - Lista de totais

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,18 @@
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  transactions: Transaction[];
+  balance: object;
+}
+
+let income: string;
+let outcome: number;
+let total: number;
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +23,55 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { transactions } = this;
+    function getTotalIncome(total, item) {
+      return total + (item.type === 'income' ? item.value : null);
+    }
+    const totalIncome = this.transactions.reduce(getTotalIncome, 0);
+
+    function getTotalOutcome(total, item) {
+      return total + (item.type === 'outcome' ? item.value : null);
+    }
+    const totalOutcome = this.transactions.reduce(getTotalOutcome, 0);
+
+    const totalTotal = totalIncome - totalOutcome;
+
+    const teste = {
+      transactions,
+      balance: {
+        income: totalIncome,
+        outcome: totalOutcome,
+        total: totalTotal,
+      },
+    };
+    return teste;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const { transactions } = this;
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    function getTotalIncome(total, item) {
+      return total + (item.type === 'income' ? item.value : null);
+    }
+    const totalIncome = this.transactions.reduce(getTotalIncome, 0);
+
+    if (type === 'outcome' && value > totalIncome) {
+      console.log('ultrapassou');
+      return err;
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const listTransactions = [];
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const teste = transactionsRepository.getBalance();
+
+    return response.status(200).json(teste);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,9 +21,17 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({ title, type, value });
+
+    return response.status(200).json(transaction);
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({ error: 'O Valor é maior que o saldo' });
   }
 });
 
